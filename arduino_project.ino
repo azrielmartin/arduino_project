696ad95c067c70820bfa64166f102b6168bbe6e2
azrielmartin <azrielmartin29@gmail.com>
2023-07-13 15:03:55 +0800

    New code

--- arduino_project.ino
+++ arduino_project.ino
@@ -1,25 +1,80 @@
-//  "a2ee973a-8e1e-4be0-b689-11704d514b72" DEVICE_ID change if new device
-//  "14444878-632a-07be-67c5-703e3f566392"; KEY 
-//  "f2c24d11-a83e-dd9b-579c-9be7228a1a26"; SECRET
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <Crypto.h>
+#include <SHA256.h>
+#include <Base64.h>
 
-// Pin connected to the LED
-
+//-----WIFI SETTINGS-----
+const char* ssid = "Azriel";
+const char* password = "theresa72";
+const char* apiUrl = "https://ojt-relay-switch-api.vercel.app";
+const char* apiKey = "14444878-632a-07be-67c5-703e3f566392";
+const String apiSecret = "f2c24d11-a83e-dd9b-579c-9be7228a1a26";
+String url = ""; // Replace this with the actual URL
+String dataA = "";
 
 void setup() {
-  // Initialize the digital pin as an output
-  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(9600);
+  delay(1000);
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi!");
+
+  // Generate the dataA string
+  url = ""; // Replace this with the actual URL
+  dataA = String(millis()) + "/" + url;
+
+  // Generate the HMAC signature with SHA256 algorithm
+  byte hmacSignature[SHA256_BLOCK_SIZE];
+  HMACSHA256 hmac((const byte*)apiSecret.c_str(), apiSecret.length());
+  hmac.doUpdate((const byte*)dataA.c_str(), dataA.length());
+  hmac.doFinal(hmacSignature);
+
+  // Convert the HMAC signature to a hexadecimal string
+  String hmacSignatureHex = "";
+  char hex[3];
+  for (int i = 0; i < SHA256_BLOCK_SIZE; i++) {
+    sprintf(hex, "%02x", hmacSignature[i]);
+    hmacSignatureHex += hex;
+  }
+
+  Serial.println(dataA);
+  Serial.println(hmacSignatureHex);
+
+  // Make API request
+  HTTPClient http;
+  http.begin(apiUrl);
+  http.addHeader("X-API-Key", apiKey);
+  http.addHeader("X-HMAC-Signature", hmacSignatureHex);
+  http.addHeader("X-Timestamp", String(millis()));
+  int httpResponseCode = http.GET();
+
+  if (httpResponseCode == HTTP_CODE_OK) {
+    String response = http.getString();
+    Serial.println("API response:");
+    Serial.println(response);
+
+    // Check if the response indicates successful access
+    if (response == "Access Granted") {
+      Serial.println("Access granted. Welcome to the application!");
+      // Perform the desired actions when access is granted
+    } else {
+      Serial.println("Access denied. Please check your API key.");
+      // Perform any necessary actions when access is denied
+    }
+  } else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+
+  http.end();
 }
 
 void loop() {
-  // Turn the LED on
-  digitalWrite(LED_PIN, HIGH);
-  
-  // Wait for 1 second
-  delay(1000);
-
-  // Turn the LED off
-  digitalWrite(LED_PIN, LOW);
-  
-  // Wait for 1 second
-  delay(1000);
+  // Your code here
 }
