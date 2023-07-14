2c06d691eba67b6b7198ba8314cb8424f1bd00cd
ragingK9 <139431498+ragingK9@users.noreply.github.com>
2023-07-14 08:44:44 +0800

    Test

--- arduino_project.ino
+++ arduino_project.ino
@@ -1,80 +1,202 @@
-#include <sha256.h>
+// Uncomment the following line to enable serial debug output
+//#define ENABLE_DEBUG
+
+#ifdef ENABLE_DEBUG
+       #define DEBUG_ESP_PORT Serial
+       #define NODEBUG_WEBSOCKETS
+       #define NDEBUG
+#endif 
+
+#include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
+#include "SinricPro.h"
+#include "SinricProSwitch.h"
+#include <map>
 #include <Crypto.h>
-#include <Base64.h>
+#include <Hash.h>
 
-//-----WIFI SETTINGS-----
-const char* ssid = "Azriel";
-const char* password = "theresa72";
-const char* apiUrl = "https://ojt-relay-switch-api.vercel.app";
-const char* apiKey = "14444878-632a-07be-67c5-703e3f566392";
-const String apiSecret = "f2c24d11-a83e-dd9b-579c-9be7228a1a26";
-String url = ""; // Replace this with the actual URL
-String dataA = "";
+#define WIFI_SSID         "YOUR-WIFI-NAME"    
+#define WIFI_PASS         "YOUR-WIFI-PASSWORD"
+#define APP_KEY           "YOUR-APP-KEY"      // Should look like "de0bxxxx-1x3x-4x3x-ax2x-5dabxxxxxxxx"
+#define APP_SECRET        "YOUR-APP-SECRET"   // Should look like "5f36xxxx-x3x7-4x3x-xexe-e86724a9xxxx-4c4axxxx-3x3x-x5xe-x9x3-333d65xxxxxx"
 
-void setup() {
-  Serial.begin(9600);
-  delay(1000);
+//Enter the device IDs here
+#define device_ID_1   "SWITCH_ID_NO_1_HERE"
+#define device_ID_2   "SWITCH_ID_NO_2_HERE"
+#define device_ID_3   "SWITCH_ID_NO_3_HERE"
+#define device_ID_4   "SWITCH_ID_NO_4_HERE"
 
-  // Connect to Wi-Fi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
+// define the GPIO connected with Relays and switches
+#define RelayPin1 5  //D1
+#define RelayPin2 4  //D2
+#define RelayPin3 14 //D5
+#define RelayPin4 12 //D6
+
+#define SwitchPin1 10  //SD3
+#define SwitchPin2 0   //D3 
+#define SwitchPin3 13  //D7
+#define SwitchPin4 3   //RX
+
+#define wifiLed   16   //D0
+
+// comment the following line if you use a toggle switches instead of tactile buttons
+//#define TACTILE_BUTTON 1
+
+#define BAUD_RATE   9600
+
+#define DEBOUNCE_TIME 250
+
+typedef struct {      // struct for the std::map below
+  int relayPIN;
+  int flipSwitchPIN;
+} deviceConfig_t;
+
+
+// this is the main configuration
+// please put in your deviceId, the PIN for Relay and PIN for flipSwitch
+// this can be up to N devices...depending on how much pin's available on your device ;)
+// right now we have 4 devicesIds going to 4 relays and 4 flip switches to switch the relay manually
+std::map<String, deviceConfig_t> devices = {
+    //{deviceId, {relayPIN,  flipSwitchPIN}}
+    {device_ID_1, {  RelayPin1, SwitchPin1 }},
+    {device_ID_2, {  RelayPin2, SwitchPin2 }},
+    {device_ID_3, {  RelayPin3, SwitchPin3 }},
+    {device_ID_4, {  RelayPin4, SwitchPin4 }}     
+};
+
+typedef struct {      // struct for the std::map below
+  String deviceId;
+  bool lastFlipSwitchState;
+  unsigned long lastFlipSwitchChange;
+} flipSwitchConfig_t;
+
+std::map<int, flipSwitchConfig_t> flipSwitches;    // this map is used to map flipSwitch PINs to deviceId and handling debounce and last flipSwitch state checks
+                                                  // it will be setup in "setupFlipSwitches" function, using informations from devices map
+
+void setupRelays() { 
+  for (auto &device : devices) {           // for each device (relay, flipSwitch combination)
+    int relayPIN = device.second.relayPIN; // get the relay pin
+    pinMode(relayPIN, OUTPUT);             // set relay pin to OUTPUT
+    digitalWrite(relayPIN, HIGH);
   }
-  Serial.println("Connected to WiFi!");
-
-  // Generate the dataA string
-  url = ""; // Replace this with the actual URL
-  dataA = String(millis()) + "/" + url;
-
-  // Generate the HMAC signature with SHA256 algorithm
-  byte hmacSignature[SHA256_BLOCK_SIZE];
-  HMACSHA256 hmac((const byte*)apiSecret.c_str(), apiSecret.length());
-  hmac.doUpdate((const byte*)dataA.c_str(), dataA.length());
-  hmac.doFinal(hmacSignature);
-
-  // Convert the HMAC signature to a hexadecimal string
-  String hmacSignatureHex = "";
-  char hex[3];
-  for (int i = 0; i < SHA256_BLOCK_SIZE; i++) {
-    sprintf(hex, "%02x", hmacSignature[i]);
-    hmacSignatureHex += hex;
-  }
-
-  Serial.println(dataA);
-  Serial.println(hmacSignatureHex);
-
-  // Make API request
-  HTTPClient http;
-  http.begin(apiUrl);
-  http.addHeader("X-API-Key", apiKey);
-  http.addHeader("X-HMAC-Signature", hmacSignatureHex);
-  http.addHeader("X-Timestamp", String(millis()));
-  int httpResponseCode = http.GET();
-
-  if (httpResponseCode == HTTP_CODE_OK) {
-    String response = http.getString();
-    Serial.println("API response:");
-    Serial.println(response);
-
-    // Check if the response indicates successful access
-    if (response == "Access Granted") {
-      Serial.println("Access granted. Welcome to the application!");
-      // Perform the desired actions when access is granted
-    } else {
-      Serial.println("Access denied. Please check your API key.");
-      // Perform any necessary actions when access is denied
-    }
-  } else {
-    Serial.print("Error code: ");
-    Serial.println(httpResponseCode);
-  }
-
-  http.end();
 }
 
-void loop() {
-  // Your code here
+void setupFlipSwitches() {
+  for (auto &device : devices)  {                     // for each device (relay / flipSwitch combination)
+    flipSwitchConfig_t flipSwitchConfig;              // create a new flipSwitch configuration
+
+    flipSwitchConfig.deviceId = device.first;         // set the deviceId
+    flipSwitchConfig.lastFlipSwitchChange = 0;        // set debounce time
+    flipSwitchConfig.lastFlipSwitchState = true;     // set lastFlipSwitchState to false (LOW)--
+
+    int flipSwitchPIN = device.second.flipSwitchPIN;  // get the flipSwitchPIN
+
+    flipSwitches[flipSwitchPIN] = flipSwitchConfig;   // save the flipSwitch config to flipSwitches map
+    pinMode(flipSwitchPIN, INPUT_PULLUP);                   // set the flipSwitch pin to INPUT
+  }
+}
+
+bool onPowerState(String deviceId, bool &state)
+{
+  Serial.printf("%s: %s\r\n", deviceId.c_str(), state ? "on" : "off");
+  int relayPIN = devices[deviceId].relayPIN; // get the relay pin for corresponding device
+  digitalWrite(relayPIN, !state);             // set the new relay state
+  return true;
+}
+
+void handleFlipSwitches() {
+  unsigned long actualMillis = millis();                                          // get actual millis
+  for (auto &flipSwitch : flipSwitches) {                                         // for each flipSwitch in flipSwitches map
+    unsigned long lastFlipSwitchChange = flipSwitch.second.lastFlipSwitchChange;  // get the timestamp when flipSwitch was pressed last time (used to debounce / limit events)
+
+    if (actualMillis - lastFlipSwitchChange > DEBOUNCE_TIME) {                    // if time is > debounce time...
+
+      int flipSwitchPIN = flipSwitch.first;                                       // get the flipSwitch pin from configuration
+      bool lastFlipSwitchState = flipSwitch.second.lastFlipSwitchState;           // get the lastFlipSwitchState
+      bool flipSwitchState = digitalRead(flipSwitchPIN);                          // read the current flipSwitch state
+      if (flipSwitchState != lastFlipSwitchState) {                               // if the flipSwitchState has changed...
+#ifdef TACTILE_BUTTON
+        if (flipSwitchState) {                                                    // if the tactile button is pressed 
+#endif      
+          flipSwitch.second.lastFlipSwitchChange = actualMillis;                  // update lastFlipSwitchChange time
+          String deviceId = flipSwitch.second.deviceId;                           // get the deviceId from config
+          int relayPIN = devices[deviceId].relayPIN;                              // get the relayPIN from config
+          bool newRelayState = !digitalRead(relayPIN);                            // set the new relay State
+          digitalWrite(relayPIN, newRelayState);                                  // set the trelay to the new state
+
+          SinricProSwitch &mySwitch = SinricPro[deviceId];                        // get Switch device from SinricPro
+          mySwitch.sendPowerStateEvent(!newRelayState);                            // send the event
+#ifdef TACTILE_BUTTON
+        }
+#endif      
+        flipSwitch.second.lastFlipSwitchState = flipSwitchState;                  // update lastFlipSwitchState
+      }
+    }
+  }
+}
+
+void setupWiFi()
+{
+  Serial.printf("\r\n[Wifi]: Connecting");
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.printf(".");
+    delay(250);
+  }
+  digitalWrite(wifiLed, LOW);
+  Serial.printf("connected!\r\n[WiFi]: IP-Address is %s\r\n", WiFi.localIP().toString().c_str());
+}
+
+void setupSinricPro()
+{
+  for (auto &device : devices)
+  {
+    const char *deviceId = device.first.c_str();
+    SinricProSwitch &mySwitch = SinricPro[deviceId];
+    mySwitch.onPowerState(onPowerState);
+  }
+
+  SinricPro.begin(APP_KEY, APP_SECRET);
+  SinricPro.restoreDeviceStates(true);
+}
+
+void setup()
+{
+  Serial.begin(BAUD_RATE);
+
+  pinMode(wifiLed, OUTPUT);
+  digitalWrite(wifiLed, HIGH);
+
+  setupRelays();
+  setupFlipSwitches();
+  setupWiFi();
+  setupSinricPro();
+
+  //CLIENT SIDE HMAC Generation method
+  const String currentTimestamp = String(millis());
+  const String apiKey = "14444878-632a-07be-67c5-703e3f566392";
+  const String apiSecret = "f2c24d11-a83e-dd9b-579c-9be7228a1a26";
+  const String url = "YOUR_RELAY_BASE_URL"; // Replace with your relay base URL
+  const String dataA = currentTimestamp + "/" + url;
+
+  // Generate the HMAC signature with SHA256 algorithm
+  const String hmacSignature = Crypto.hmacSha256(dataA, apiSecret).toString();
+  Serial.println(dataA);
+  Serial.println(hmacSignature);
+
+  // Add headers to the request
+  String headers = "X-API-Key: " + apiKey + "\r\n";
+  headers += "X-HMAC-Signature: " + hmacSignature + "\r\n";
+  headers += "X-Timestamp: " + currentTimestamp + "\r\n";
+
+  // Uncomment the following line to add the headers to the request
+  // client.addHeader(headers);
+}
+
+void loop()
+{
+  SinricPro.handle();
+  handleFlipSwitches();
 }
