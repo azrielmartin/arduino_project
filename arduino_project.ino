aff44823bbd50b6b4e306225cdf602b35b6bffc7
ragingK9 <139431498+ragingK9@users.noreply.github.com>
2023-07-17 09:37:44 +0800

    Testing 6

--- arduino_project.ino
+++ arduino_project.ino
@@ -1,172 +1,42 @@
-// Uncomment the following line to enable serial debug output
-//#define ENABLE_DEBUG
-//AziAgi
-#ifdef ENABLE_DEBUG
-       #define DEBUG_ESP_PORT Serial
-       #define NODEBUG_WEBSOCKETS
-       #define NDEBUG
-#endif 
-
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include <map>
-#include <Crypto.h>
-#include <Hash.h>
+#include <ArduinoCrypto.h>
+//Azi Gwapo
 
-#define WIFI_SSID         "YOUR-WIFI-NAME"    
-#define WIFI_PASS         "YOUR-WIFI-PASSWORD"
+//CLIENT SIDE HMAC Generation method
+unsigned long currentTimestamp = millis();
+const char* apiKey = "14444878-632a-07be-67c5-703e3f566392";
+const char* apiSecret = "f2c24d11-a83e-dd9b-579c-9be7228a1a26";
+String url = "YOUR_URL"; // Replace "YOUR_URL" with the desired URL
 
-//Enter the device IDs here
-#define device_ID_1   "SWITCH_ID_NO_1_HERE"
-#define device_ID_2   "SWITCH_ID_NO_2_HERE"
-#define device_ID_3   "SWITCH_ID_NO_3_HERE"
-#define device_ID_4   "SWITCH_ID_NO_4_HERE"
+String dataA = String(currentTimestamp) + "/" + url;
 
-// define the GPIO connected with Relays and switches
-#define RelayPin1 5  //D1
-#define RelayPin2 4  //D2
-#define RelayPin3 14 //D5
-#define RelayPin4 12 //D6
-
-#define SwitchPin1 10  //SD3
-#define SwitchPin2 0   //D3 
-#define SwitchPin3 13  //D7
-#define SwitchPin4 3   //RX
-
-#define wifiLed   16   //D0
-
-// comment the following line if you use a toggle switches instead of tactile buttons
-//#define TACTILE_BUTTON 1
-
-#define BAUD_RATE   9600
-
-#define DEBOUNCE_TIME 250
-
-typedef struct {      // struct for the std::map below
-  int relayPIN;
-  int flipSwitchPIN;
-} deviceConfig_t;
-
-
-// this is the main configuration
-// please put in your deviceId, the PIN for Relay and PIN for flipSwitch
-// this can be up to N devices...depending on how much pin's available on your device ;)
-// right now we have 4 devicesIds going to 4 relays and 4 flip switches to switch the relay manually
-std::map<String, deviceConfig_t> devices = {
-    //{deviceId, {relayPIN,  flipSwitchPIN}}
-    {device_ID_1, {  RelayPin1, SwitchPin1 }},
-    {device_ID_2, {  RelayPin2, SwitchPin2 }},
-    {device_ID_3, {  RelayPin3, SwitchPin3 }},
-    {device_ID_4, {  RelayPin4, SwitchPin4 }}     
-};
-
-typedef struct {      // struct for the std::map below
-  String deviceId;
-  bool lastFlipSwitchState;
-  unsigned long lastFlipSwitchChange;
-} flipSwitchConfig_t;
-
-std::map<int, flipSwitchConfig_t> flipSwitches;    // this map is used to map flipSwitch PINs to deviceId and handling debounce and last flipSwitch state checks
-                                                  // it will be setup in "setupFlipSwitches" function, using informations from devices map
-
-void setupRelays() { 
-  for (auto &device : devices) {           // for each device (relay, flipSwitch combination)
-    int relayPIN = device.second.relayPIN; // get the relay pin
-    pinMode(relayPIN, OUTPUT);             // set relay pin to OUTPUT
-    digitalWrite(relayPIN, HIGH);
-  }
-}
-
-void setupFlipSwitches() {
-  for (auto &device : devices)  {                     // for each device (relay / flipSwitch combination)
-    flipSwitchConfig_t flipSwitchConfig;              // create a new flipSwitch configuration
-
-    flipSwitchConfig.deviceId = device.first;         // set the deviceId
-    flipSwitchConfig.lastFlipSwitchChange = 0;        // set debounce time
-    flipSwitchConfig.lastFlipSwitchState = true;     // set lastFlipSwitchState to false (LOW)--
-
-    int flipSwitchPIN = device.second.flipSwitchPIN;  // get the flipSwitchPIN
-
-    flipSwitches[flipSwitchPIN] = flipSwitchConfig;   // save the flipSwitch config to flipSwitches map
-    pinMode(flipSwitchPIN, INPUT_PULLUP);                   // set the flipSwitch pin to INPUT
-  }
-}
-
-void handleFlipSwitches() {
-  unsigned long actualMillis = millis();                                          // get actual millis
-  for (auto &flipSwitch : flipSwitches) {                                         // for each flipSwitch in flipSwitches map
-    unsigned long lastFlipSwitchChange = flipSwitch.second.lastFlipSwitchChange;  // get the timestamp when flipSwitch was pressed last time (used to debounce / limit events)
-
-    if (actualMillis - lastFlipSwitchChange > DEBOUNCE_TIME) {                    // if time is > debounce time...
-
-      int flipSwitchPIN = flipSwitch.first;                                       // get the flipSwitch pin from configuration
-      bool lastFlipSwitchState = flipSwitch.second.lastFlipSwitchState;           // get the lastFlipSwitchState
-      bool flipSwitchState = digitalRead(flipSwitchPIN);                          // read the current flipSwitch state
-      if (flipSwitchState != lastFlipSwitchState) {                               // if the flipSwitchState has changed...
-#ifdef TACTILE_BUTTON
-        if (flipSwitchState) {                                                    // if the tactile button is pressed 
-#endif      
-          flipSwitch.second.lastFlipSwitchChange = actualMillis;                  // update lastFlipSwitchChange time
-          String deviceId = flipSwitch.second.deviceId;                           // get the deviceId from config
-          int relayPIN = devices[deviceId].relayPIN;                              // get the relayPIN from config
-          bool newRelayState = !digitalRead(relayPIN);                            // set the new relay State
-          digitalWrite(relayPIN, newRelayState);                                  // set the trelay to the new state
-#ifdef TACTILE_BUTTON
-        }
-#endif      
-        flipSwitch.second.lastFlipSwitchState = flipSwitchState;                  // update lastFlipSwitchState
-      }
-    }
-  }
-}
-
-void setupWiFi()
-{
-  Serial.printf("\r\n[Wifi]: Connecting");
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
-
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.printf(".");
-    delay(250);
-  }
-  digitalWrite(wifiLed, LOW);
-  Serial.printf("connected!\r\n[WiFi]: IP-Address is %s\r\n", WiFi.localIP().toString().c_str());
-}
-
-void setup()
-{
-  Serial.begin(BAUD_RATE);
-
-  pinMode(wifiLed, OUTPUT);
-  digitalWrite(wifiLed, HIGH);
-
-  setupRelays();
-  setupFlipSwitches();
-  setupWiFi();
-
-  //CLIENT SIDE HMAC Generation method
-  const unsigned long currentTimestamp = millis();
-  const String apiKey = "14444878-632a-07be-67c5-703e3f566392";
-  const String apiSecret = "f2c24d11-a83e-dd9b-579c-9be7228a1a26";
-  const String url = "YOUR_RELAY_BASE_URL"; // Replace with your relay base URL
-  const String dataA = String(currentTimestamp) + "/" + url;
+void setup() {
+  Serial.begin(9600);
 
   // Generate the HMAC signature with SHA256 algorithm
-  const String hmacSignature = Crypto.hmacSha256(dataA, apiSecret).toString();
+  CryptoSHA256 sha256;
+  sha256.update((const uint8_t*)apiSecret, strlen(apiSecret));
+  sha256.update((const uint8_t*)dataA.c_str(), dataA.length());
+  uint8_t hmacSignature[CryptoSHA256::HASH_SIZE];
+  sha256.finalize(hmacSignature);
+
+  String hmacSignatureString = "";
+  for (int i = 0; i < CryptoSHA256::HASH_SIZE; i++) {
+    if (hmacSignature[i] < 0x10) {
+      hmacSignatureString += "0";
+    }
+    hmacSignatureString += String(hmacSignature[i], HEX);
+  }
+
   Serial.println(dataA);
-  Serial.println(hmacSignature);
+  Serial.println(hmacSignatureString);
 
-  // Add headers to the request
-  String headers = "X-API-Key: " + apiKey + "\r\n";
-  headers += "X-HMAC-Signature: " + hmacSignature + "\r\n";
-  headers += "X-Timestamp: " + String(currentTimestamp) + "\r\n";
-
-  // Uncomment the following lines to add the headers to the request
-  // client.addHeader(headers);
+  // Set request headers
+  Serial.println("X-API-Key: " + String(apiKey));
+  Serial.println("X-HMAC-Signature: " + hmacSignatureString);
+  Serial.println("X-Timestamp: " + String(currentTimestamp));
 }
 
-void loop()
-{
-  handleFlipSwitches();
+void loop() {
+  // Your code here
 }
