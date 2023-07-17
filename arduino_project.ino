19545aa7a696aa9ddc485008262e7a8324f8bf8e
ragingK9 <139431498+ragingK9@users.noreply.github.com>
2023-07-17 10:56:57 +0800

    Testing 7

--- arduino_project.ino
+++ arduino_project.ino
@@ -1,40 +1,45 @@
 #include <Arduino.h>
-#include <ArduinoCrypto.h>
-//Azi Gwapo
+#include <Crypto.h>
+#include <CryptoLib.h>
 
-//CLIENT SIDE HMAC Generation method
-unsigned long currentTimestamp = millis();
 const char* apiKey = "14444878-632a-07be-67c5-703e3f566392";
 const char* apiSecret = "f2c24d11-a83e-dd9b-579c-9be7228a1a26";
-String url = "YOUR_URL"; // Replace "YOUR_URL" with the desired URL
-
-String dataA = String(currentTimestamp) + "/" + url;
+char url[] = "{{RELAY_BASE_URL}}"; // Replace with the actual URL
+char dataA[128];
 
 void setup() {
   Serial.begin(9600);
+  
+  // Generate current timestamp
+  unsigned long currentTimestamp = millis();
+
+  // Construct dataA
+  sprintf(dataA, "%lu/%s", currentTimestamp, url);
 
   // Generate the HMAC signature with SHA256 algorithm
-  CryptoSHA256 sha256;
-  sha256.update((const uint8_t*)apiSecret, strlen(apiSecret));
-  sha256.update((const uint8_t*)dataA.c_str(), dataA.length());
-  uint8_t hmacSignature[CryptoSHA256::HASH_SIZE];
-  sha256.finalize(hmacSignature);
+  byte hmacSignature[32];
+  Sha256.initHmac((const uint8_t*)apiSecret, strlen(apiSecret));
+  Sha256.print(dataA);
+  Sha256.getHmacResult(hmacSignature, true);
 
-  String hmacSignatureString = "";
-  for (int i = 0; i < CryptoSHA256::HASH_SIZE; i++) {
-    if (hmacSignature[i] < 0x10) {
-      hmacSignatureString += "0";
-    }
-    hmacSignatureString += String(hmacSignature[i], HEX);
-  }
-
+  // Print dataA and hmacSignature
   Serial.println(dataA);
-  Serial.println(hmacSignatureString);
+  Serial.println();
 
-  // Set request headers
-  Serial.println("X-API-Key: " + String(apiKey));
-  Serial.println("X-HMAC-Signature: " + hmacSignatureString);
-  Serial.println("X-Timestamp: " + String(currentTimestamp));
+  Serial.print("HMAC Signature: ");
+  for (int i = 0; i < sizeof(hmacSignature); i++) {
+    if (hmacSignature[i] < 0x10) {
+      Serial.print("0");
+    }
+    Serial.print(hmacSignature[i], HEX);
+  }
+  Serial.println();
+
+  // Add headers to the request
+  // Replace "client" with the appropriate client library you are using for sending HTTP requests
+  client.addHeader("X-API-Key", apiKey);
+  client.addHeader("X-HMAC-Signature", hmacSignature);
+  client.addHeader("X-Timestamp", String(currentTimestamp));
 }
 
 void loop() {
