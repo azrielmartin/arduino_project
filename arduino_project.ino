6d78451f42220ada2764f64f476b503e272f6844
ragingK9 <139431498+ragingK9@users.noreply.github.com>
2023-07-17 11:02:50 +0800

    Testing 7

--- arduino_project.ino
+++ arduino_project.ino
@@ -1,6 +1,5 @@
 #include <Arduino.h>
-#include <Crypto.h>
-#include <CryptoLib.h>
+#include <ArduinoCrypto.h>
 
 const char* apiKey = "14444878-632a-07be-67c5-703e3f566392";
 const char* apiSecret = "f2c24d11-a83e-dd9b-579c-9be7228a1a26";
@@ -9,7 +8,7 @@ char dataA[128];
 
 void setup() {
   Serial.begin(9600);
-  
+
   // Generate current timestamp
   unsigned long currentTimestamp = millis();
 
@@ -17,10 +16,8 @@ void setup() {
   sprintf(dataA, "%lu/%s", currentTimestamp, url);
 
   // Generate the HMAC signature with SHA256 algorithm
-  byte hmacSignature[32];
-  Sha256.initHmac((const uint8_t*)apiSecret, strlen(apiSecret));
-  Sha256.print(dataA);
-  Sha256.getHmacResult(hmacSignature, true);
+  uint8_t hmacSignature[32];
+  hmac_sha256((const uint8_t*)apiSecret, strlen(apiSecret), (const uint8_t*)dataA, strlen(dataA), hmacSignature);
 
   // Print dataA and hmacSignature
   Serial.println(dataA);
@@ -34,12 +31,6 @@ void setup() {
     Serial.print(hmacSignature[i], HEX);
   }
   Serial.println();
-
-  // Add headers to the request
-  // Replace "client" with the appropriate client library you are using for sending HTTP requests
-  client.addHeader("X-API-Key", apiKey);
-  client.addHeader("X-HMAC-Signature", hmacSignature);
-  client.addHeader("X-Timestamp", String(currentTimestamp));
 }
 
 void loop() {
