0e7103a0089ae888d78a113b0c6d86f9c7e8e7ed
azrielmartin <azrielmartin29@gmail.com>
2023-07-21 15:09:57 +0800

    Need to get data from JSONDocument

--- arduino_project.ino
+++ arduino_project.ino
@@ -1,16 +1,20 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
 #include <ArduinoJson.h> //6.10.1
- 
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+
+const int timeZoneOffset = 8;
+
 const char* ssid = "Azriel";
 const char* password = "theresa72";
 const char* host = "ojt-relay-switch-api.vercel.app";
 const char* deviceID = "7198ec46-0d2e-49a9-a9d5-121058cfc425";
 const int httpsPort = 443;
-const int relay1Pin = 5;
-const int relay2Pin = 4;
-const int relay3Pin = 0;
-const int relay4Pin = 2;
+const int relay1Pin = D1;
+const int relay2Pin = D2;
+const int relay3Pin = D3;
+const int relay4Pin = D4;
 // char* data_channel[3];
 
 bool relay1State = false;
@@ -19,6 +23,8 @@ bool relay3State = false;
 bool relay4State = false;
 
 WiFiClientSecure client;
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", timeZoneOffset);
 
 void setup() {
   Serial.begin(115200);
@@ -30,6 +36,7 @@ void setup() {
     Serial.println("Connecting to WiFi...");
   }
   Serial.println("Connected to WiFi");
+  timeClient.begin();
 
   pinMode(relay1Pin, OUTPUT);
   pinMode(relay2Pin, OUTPUT);
@@ -38,6 +45,7 @@ void setup() {
 }
 
 void loop() {
+  timeClient.update();
   client.setInsecure();
 
   Serial.print("Connecting to ");
@@ -59,19 +67,16 @@ void getAmIActive() {
   client.println();
 
   Serial.println("Request sent.");
-  while (client.connected()){
-    String line = client.readStringUntil('\n');
-    Serial.println(line);
 
-    if (line == "\r") {
-      break;
-    }
+  while (client.connected() && !client.available()){
+    delay(10);
   }
 
+  // Read the response
   String jsonPayload;
   while (client.available()) {
-    jsonPayload = client.readStringUntil('\n');
-    Serial.println(jsonPayload);
+    char c = client.read();
+    jsonPayload += c;
   }
 
   StaticJsonDocument<256> jsonDocument;
@@ -82,7 +87,7 @@ void getAmIActive() {
     Serial.println(error.c_str());
   } else {
     char* data;
-    bool activated = false;
+    
 
     if (jsonDocument.containsKey("error")) {
       data = strdup(jsonDocument["error"].as<char*>());
@@ -102,9 +107,6 @@ void getAmIActive() {
       Serial.print("Data: ");
       Serial.println(data);
       free(data);
-    }
-    if(!activated){
-      getMyChannelStatus;
     }
   }
 
@@ -132,31 +134,99 @@ void getMyChannelStatus() {
 
   String jsonPayload;
   while (client.available()) {
-    jsonPayload = client.readStringUntil('\n');
-    Serial.println(jsonPayload);
+    char c = client.read();
+    jsonPayload += c;
   }
 
-// Print the received JSON payload before parsing
-  Serial.println("Received JSON Payload:");
+  // Print the received JSON response
+  Serial.println("Received JSON response:");
   Serial.println(jsonPayload);
 
+  // Parse the JSON response
   StaticJsonDocument<256> jsonDocument;
   DeserializationError error = deserializeJson(jsonDocument, jsonPayload);
 
   if (error) {
     Serial.print("Error parsing JSON: ");
     Serial.println(error.c_str());
-  } else if (!jsonPayload.isEmpty()) {
+  } 
+  
+  
+  else if (!jsonPayload.isEmpty()) {
     Serial.println("Channel Status:");
 
-    JsonArray statusArray = jsonDocument["status"].as<JsonArray>();
+    int currentHour = timeClient.getHours();
+    int currentMinute = timeClient.getMinutes();
+
+    JsonArray statusArray = jsonDocument[jsonPayload].as<JsonArray>();
+    // JsonArray dataArray = jsonDocument.createNestedArray("data");
+    // Serial.println(jsonDocument);
+
+    const char* name1;
+    bool statusValue1;
+    const char* date1;
+    const char* time1;
+
+    const char* name2;
+    bool statusValue2;
+    const char* date2;
+    const char* time2;
+
+    const char* name3;
+    bool statusValue3;
+    const char* date3;
+    const char* time3;
+
+    const char* name4;
+    bool statusValue4;
+    const char* date4;
+    const char* time4;
+
+    // size_t jsonLength = measureJson(jsonDocument);
+
 
     for (int i = 0; i < statusArray.size(); i++) {
       JsonObject status = statusArray[i];
-      const char* name = status["name"];
+
+      // Access individual elements of each "status" object
+      const char* name = status["name"].as<const char*>();
       bool statusValue = status["status"];
-      const char* date = status["timeout"]["date"];
-      const char* time = status["timeout"]["time"];
+
+      JsonObject timeoutObject = status["timeout"]; // Access the "timeout" object within the status object
+      const char* date = timeoutObject["date"].as<const char*>();
+      const char* time = timeoutObject["time"].as<const char*>();
+
+      if(i == 0){
+        // put values to 1
+        name1 = name;
+        statusValue1 = statusValue;
+        date1 = date;
+        time1 = time;
+      }
+      else  if(i == 1){
+        // put values to 2
+        name2 = name;
+        statusValue2 = statusValue;
+        date2 = date;
+        time2 = time;
+      }
+      else  if(i == 2){
+        // put values to 3
+        name3 = name;
+        statusValue3 = statusValue;
+        date3 = date;
+        time3 = time;
+      }
+      else  if(i == 3){
+        name4 = name;
+        statusValue4 = statusValue;
+        date4 = date;
+        time4 = time;
+      } 
+      else {
+        Serial.print("Error");
+        client.stop();
+      }
 
       // Print the channel status data
       Serial.print("Name: ");
@@ -164,34 +234,65 @@ void getMyChannelStatus() {
       Serial.print("Status: ");
       Serial.println(statusValue);
       Serial.print("Date: ");
-      Serial.println(date ? date : "null");
+      Serial.println(date);
       Serial.print("Time: ");
-      Serial.println(time ? time : "null");
+      Serial.println(time);
       Serial.println();
-
-      // Update relay states based on channel status
-      if (i == 0) {
-        relay1State = statusValue;
-        digitalWrite(relay1Pin, statusValue ? HIGH : LOW);
-      } else if (i == 1) {
-        relay2State = statusValue;
-        digitalWrite(relay2Pin, statusValue ? HIGH : LOW);
-      } else if (i == 2) {
-        relay3State = statusValue;
-        digitalWrite(relay3Pin, statusValue ? HIGH : LOW);
-      } else if (i == 3) {
-        relay4State = statusValue;
-        digitalWrite(relay4Pin, statusValue ? HIGH : LOW);
-      } else {
-        // Handle an unexpected number of channels (if needed)
-      }
     }
 
-    // Print the JSON document in a pretty format
-    String jsonPretty;
-    serializeJsonPretty(jsonDocument, jsonPretty);
-    Serial.println("JSON Document:");
-    Serial.println(jsonPretty);
+      //Channel 1
+      if (statusValue1 == true) {
+        relay1State = true;
+        if(currentHour > atoi(time1) || (currentHour == atoi(time1) && currentMinute >= atoi(time1 + 3))){
+          relay1State = false;
+          digitalWrite(relay1Pin, LOW);
+        }
+        digitalWrite(relay1Pin, HIGH); //transfer to pins as signals
+      } 
+      else if (statusValue1 == false) {
+        relay1State = false;
+      }
+      
+
+      //Switch logic
+      //Channel 2
+      if (statusValue2 == true) {
+        relay2State = true;
+        if(currentHour > atoi(time2) || (currentHour == atoi(time2) && currentMinute >= atoi(time2 + 3))){
+          relay2State = false;
+          digitalWrite(relay2Pin, LOW);
+        }
+        digitalWrite(relay2Pin, HIGH); //transfer to pins as signals
+      } 
+      else if (statusValue2 == false) {
+        relay2State = false;
+      }
+
+      //Channel 3
+      if (statusValue3 == true) {
+        relay3State = true;
+        if(currentHour > atoi(time3) || (currentHour == atoi(time3) && currentMinute >= atoi(time3 + 3))){
+          relay3State = false;
+          digitalWrite(relay3Pin, LOW);
+        }
+        digitalWrite(relay3Pin, HIGH); //transfer to pins as signals
+      } 
+      else if (statusValue3 == false) {
+        relay3State = false;
+      }
+      
+      //Channel 4
+      if (statusValue4 == true) {
+        relay4State = true;
+        if(currentHour > atoi(time4) || (currentHour == atoi(time4) && currentMinute >= atoi(time4 + 3))){
+          relay4State = false;
+          digitalWrite(relay4Pin, LOW);
+        }
+        digitalWrite(relay4Pin, HIGH); //transfer to pins as signals
+      } 
+      else if (statusValue4 == false) {
+        relay4State = false;
+      }
   }
 
   Serial.println("Request complete.");
